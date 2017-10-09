5fd0f194a9b6f4cc9c298b637fca67864e951e66
老A <mali2ace@163.com>
2017-10-10 07:21:46 +0800

    [Feature] 2.1UI

--- AG-Admin-v2-UI/config/dev.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    NODE_ENV: '"development"',
-    BASE_API: '"http://localhost:8765"',
-    APP_ORIGIN: '"https://wallstreetcn.com"'
-}
--- AG-Admin-v2/README.md
+++
@@ -1,5 +0,0 @@
-# AG-Admin-v2
-
-# 启动方式
-
-参考[1.0](http://git.oschina.net/geek_qi/ace-security)
